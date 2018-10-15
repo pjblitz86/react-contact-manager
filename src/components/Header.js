e8cdef701fa42f1cc2ffd0823e3555790836c94b
Paulius Juknis <paulius.juknis@gmail.com>
2018-10-15 16:37:33 +0300

    proptypes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Header = props => {
   return (
@@ -6,6 +7,16 @@ const Header = props => {
       <h1>{props.branding}</h1>
     </div>
   );
+};
+
+Header.defaultProps = {
+  // if no props are passed
+  branding: 'My App'
+};
+
+Header.propTypes = {
+  // proptypes enforce types
+  branding: PropTypes.string.isRequired
 };
 
 export default Header;
