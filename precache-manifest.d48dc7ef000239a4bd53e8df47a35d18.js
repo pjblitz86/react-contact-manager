1e5b35c62a32b0367581edaba94643cad402bca6
Paulius Juknis <paulius.juknis@gmail.com>
2018-11-02 16:37:21 +0200

    Updates

---
+++ precache-manifest.d48dc7ef000239a4bd53e8df47a35d18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ac3c48b9cdbff86df9e4",
+    "url": "/react-contact-manager/static/js/runtime~main.ac3c48b9.js"
+  },
+  {
+    "revision": "184d7d33291083c75bf0",
+    "url": "/react-contact-manager/static/js/main.184d7d33.chunk.js"
+  },
+  {
+    "revision": "bd5340dc400e741ec2fc",
+    "url": "/react-contact-manager/static/js/1.bd5340dc.chunk.js"
+  },
+  {
+    "revision": "184d7d33291083c75bf0",
+    "url": "/react-contact-manager/static/css/main.5bf5a46e.chunk.css"
+  },
+  {
+    "revision": "bd5340dc400e741ec2fc",
+    "url": "/react-contact-manager/static/css/1.ae65cbb2.chunk.css"
+  },
+  {
+    "revision": "d1360441af0dfe765502f68afbfc1fbb",
+    "url": "/react-contact-manager/index.html"
+  }
+];
