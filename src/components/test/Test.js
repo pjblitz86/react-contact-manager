8ecf11c6e15f52ae44988787fd845afde666f034
Paulius Juknis <paulius.juknis@gmail.com>
2018-11-02 14:49:45 +0200

    adding GET request with axios

--- src/components/test/Test.js
+++ src/components/test/Test.js
@@ -2,25 +2,33 @@ import React, { Component } from 'react';
 
 class Test extends Component {
   // Testing life-cycle hooks
+
+  state = {
+    title: '',
+    body: ''
+  };
+
   componentDidMount() {
-    console.log('componentDidMount');
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then(response => response.json())
+      .then(data => this.setState({ title: data.title, body: data.body }));
   }
 
-  componentWillMount() {
-    console.log('componentWillMount');
-  }
+  // componentWillMount() {
+  //   console.log('componentWillMount');
+  // }
 
-  componentDidUpdate() {
-    console.log('componentDidUpdate');
-  }
+  // componentDidUpdate() {
+  //   console.log('componentDidUpdate');
+  // }
 
-  componentWillUpdate() {
-    console.log('componentWillUpdate');
-  }
+  // componentWillUpdate() {
+  //   console.log('componentWillUpdate');
+  // }
 
-  componentWillReceiveProps(nextProps, nextState) {
-    console.log('componentWillReceiveProps'); // used with redux mostly, but being deprecated
-  }
+  // componentWillReceiveProps(nextProps, nextState) {
+  //   console.log('componentWillReceiveProps'); // used with redux mostly, but being deprecated
+  // }
 
   // static getDerivedStateFromProps(nextProps, nextState) {
   //   // this will be used more instead of componentWillReceiveProps
@@ -30,9 +38,11 @@ class Test extends Component {
   // static getSnapshotBeforeUpdate(prevProps, prevState) {} // also new incoming
 
   render() {
+    const { title, body } = this.state;
     return (
       <div>
-        <h1>Test Component</h1>
+        <h1>{title}</h1>
+        <p>{body}</p>
       </div>
     );
   }
--- src/context.js
+++ src/context.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 const Context = React.createContext();
 const reducer = (state, action) => {
@@ -22,28 +23,15 @@ const reducer = (state, action) => {
 
 export class Provider extends Component {
   state = {
-    contacts: [
-      {
-        id: 1,
-        name: 'John Doe',
-        email: 'jdoe@gmail.com',
-        phone: '123456'
-      },
-      {
-        id: 2,
-        name: 'Karen Williams',
-        email: 'karen@gmail.com',
-        phone: '6512345'
-      },
-      {
-        id: 3,
-        name: 'Henry Johnson',
-        email: 'henry@gmail.com',
-        phone: '789456'
-      }
-    ],
+    contacts: [],
     dispatch: action => this.setState(state => reducer(state, action))
   };
+
+  componentDidMount() {
+    axios
+      .get('https://jsonplaceholder.typicode.com/users')
+      .then(res => this.setState({ contacts: res.data }));
+  }
 
   render() {
     return (
