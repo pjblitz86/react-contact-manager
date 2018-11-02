4fa57c5ede09c879e7788559db3a189041c8dec8
Paulius Juknis <paulius.juknis@gmail.com>
2018-11-02 16:14:25 +0200

    implemented PUT

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import NotFound from './components/pages/NotFound';
 import Test from './components/test/Test';
+import EditContact from './components/contacts/EditContact';
 
 class App extends Component {
   render() {
@@ -21,6 +22,7 @@ class App extends Component {
               <Switch>
                 <Route exact path="/" component={Contacts} />
                 <Route exact path="/contact/add" component={AddContact} />
+                <Route exact path="/contact/edit/:id" component={EditContact} />
                 <Route exact path="/about" component={About} />
                 <Route exact path="/test" component={Test} />
                 <Route component={NotFound} />
